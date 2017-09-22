0d28c443238c17b0129a20f481f56ff4988a90be
Javier Jimenez <javierrcc522@gmail.com>
2017-09-22 11:33:18 -0700

    add 7 passing test by adding the class method all for the volunteer class

---
+++ lib/volunteer.rb
@@ -0,0 +1,22 @@
+class Volunteer
+  attr_reader(:name, :project_id,  :id)
+
+  def initialize(attributes)
+    @name = attributes.fetch(:name)
+    @project_id = attributes.fetch(:project_id)
+    @id = attributes.fetch(:id)
+  end
+
+  def self.all
+    returned_volunteers = DB.exec("SELECT * FROM volunteers;")
+    volunteers = []
+    returned_volunteers.each() do |volunteer|
+      name = volunteer.fetch("name")
+      project_id = volunteer.fetch("project_id").to_i()
+      id = volunteer.fetch("id").to_i()
+      volunteers.push(Volunteer.new({:name => name, :project_id => project_id, :id => id}))
+    end
+    volunteers
+  end
+
+end
